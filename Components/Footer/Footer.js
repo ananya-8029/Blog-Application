85719d234d4fcf6b59451639a88f0f39542434da
Ananya Gupta <anaya29003@gmail.com>
2023-11-01 19:22:07 +0530

    1-Oct

---
+++ Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <>
+      <div className={styles.footer}>
+        <div className={styles.logo}>lOGO.</div>
+        <div className={styles.content}></div>
+      </div>
+    </>
+  )
+}
+
+export default Footer
--- pages/intro/index.js
+++ pages/intro/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import Navbar from '@/Components/Navbar/Navbar';
+import React from "react";
+import Navbar from "@/Components/Navbar/Navbar";
+import Footer from "@/Components/Footer/Footer";
+import styles from "./index.module.css";
 
 const Home = () => {
+  const posts = [
+    {
+      id: 1,
+      title:
+        "orem Ipsum is simply dummy text of the printing a Ipsum has been the ",
+      desc: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 2,
+      title:
+        "orem Ipsum is simply dummy text of the printing and typesettinand",
+      desc: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 3,
+      title:
+        "orem Ipsum is simply dummy text of the printing and typesetting industry.",
+      desc: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum h text ever since the 1500s,",
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 4,
+      title:
+        "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum ,",
+      desc: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+      img: "https://picsum.photos/200/300",
+    },
+  ];
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className={styles.maincontainer}>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <div className={styles.post} key={post.id}>
+              <div className="postImg">
+                <img src={post.img}></img>
+              </div>
+              <div className="content">
+                <h1>{post.title}</h1>
+                <p>{post.desc}</p>
+              </div>
+              <button>Read More</button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
