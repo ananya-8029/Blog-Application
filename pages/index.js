c6ce09d9b095298e748380b066f5d05098272532
Ananya Gupta <anaya29003@gmail.com>
2023-11-18 14:58:36 +0530

    18-Nov

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Intro from "./intro/index.js";
+import Register from "./register/index.js";
 // import { useRouter } from "next/router";
 
 export default function Home() {
   // const router = useRouter();
   return (
     <main>
-      <Intro/>
+      <Register/>
+      {/* <Intro/> */}
       {/* <button onClick={()=>router.push("/login")}>Login</button> */}
     </main>
   );
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import styles from "./index.module.css";
 
 const Register = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className={styles.registerpage}>
+        <div className={styles.section1}>
+          <h1>Register</h1>
+          <form>
+            <input requried type="text" placeholder="Enter your Username" />
+            <input requried type="email" placeholder="Enter your Email" />
+            <input required type="password" placeholder="Enter your Password" />
 
-export default Register
+            <button>REGISTER</button>
+
+            <div>
+              <p>Have an account already?</p>
+              <button>Login</button>
+            </div>
+          </form>
+        </div>
+        <div className={styles.section2}></div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
