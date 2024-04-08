ff7136df541aec8062c027e1a75a55ee47f95834
Ananya Gupta <anaya29003@gmail.com>
2024-04-08 15:41:39 +0530

    Register Page

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,20 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
+import axios from "axios";
 
 const Register = () => {
+  const [intials, setInitials] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInitials((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "localhost:8800/api/auth/register",
+        intials
+      );
+    } catch (error) {
+      console.log(error)
+    }
+  };
   return (
     <>
       <div className={styles.registerpage}>
         <div className={styles.section1}>
-          <h1>Register</h1>
-          <form>
-            <input requried type="text" placeholder="Enter your Username" />
-            <input requried type="email" placeholder="Enter your Email" />
-            <input required type="password" placeholder="Enter your Password" />
+          <h1 className={styles.h1}>Register</h1>
+          <form className={styles.form}>
+            <div className={styles.inputfield}>
+              <label>Username</label>
+              <input
+                name="username"
+                className={styles.input}
+                requried
+                type="text"
+                placeholder="Enter your Username"
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.inputfieldemail}>
+              <label>Email</label>
+              <input
+                name="email"
+                className={styles.input}
+                requried
+                type="email"
+                placeholder="Enter your Email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.inputfieldpassword}>
+              <label>Password</label>
+              <input
+                name="password"
+                className={styles.input}
+                required
+                type="password"
+                placeholder="Enter your Password"
+                onChange={handleChange}
+              />
+            </div>
 
-            <button>REGISTER</button>
+            <button onClick={handleSubmit} className={styles.btnreg}>
+              REGISTER
+            </button>
 
-            <div>
+            <div className={styles.login}>
               <p>Have an account already?</p>
               <button>Login</button>
             </div>
